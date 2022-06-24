f7b9290e499b6a27f84e13d897ec64b545de9b12
Mihail Mishkin <mihail.mishkin89@gmail.com>
2022-06-24 18:05:45 +0300

    Начало д/з

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,31 @@
+import {  Route } from 'react-router-dom';
+import AppBar from './AppBar';
+import Container from './Container';
+// import Cast from './Pages/Cast';
+import Home from './Pages/Home';
+// import MovieDetails from './Pages/MovieDetails';
+import Movies from './Pages/Movies';
+// import Reviews from './Pages/Reviews';
+import NotFoundViews from './Pages/NotFoundViews';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Container>
+      <AppBar />
+
+      {/* <Switch> */}
+        <Route path="/" exact>
+          <Home />
+        </Route>
+
+        <Route path="/movies">
+          <Movies />
+        </Route>
+
+        <Route>
+          <NotFoundViews />
+        </Route>
+      {/* </Switch> */}
+    </Container>
   );
 };
